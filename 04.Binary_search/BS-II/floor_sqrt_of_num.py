ea3e1dfca876319c4ea0d92b4eda61f0c5a49f8e
shreya-212 <shreyaa.srs@gmail.com>
2026-02-04 23:07:16 +0530

    Add binary-search

---
+++ 04.Binary_search/BS-II/floor_sqrt_of_num.py
@@ -0,0 +1,35 @@
+# Given a positive integer n. Find and return its square root. If n is not a perfect square, 
+# then return the floor value of sqrt(n).
+
+
+
+
+#Brute force solution  -Time complexity:O(n)  ,space complexity:O(1)
+class Solution:
+    def floorSqrt(self, n: int) -> int:
+        ans=0
+        for i in range(1,n):
+            if i*i<=n:
+                ans=i
+            else:
+                break
+        return ans
+
+
+
+
+
+#Optimal solution  -Time complexity:O(log n)  ,space complexity:O(1)
+class Solution:
+    def floorSqrt(self, n: int) -> int:
+        low,high=0,n
+        ans=0
+        while low<=high:
+            mid=(low+high)//2
+            if mid*mid<=n:
+                ans=mid
+                low=mid+1
+            else:
+                high=mid-1
+        return ans
+
