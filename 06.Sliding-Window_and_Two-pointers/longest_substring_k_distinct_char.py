6c5d9898c4c1fa921feb0ea76382d924d2c26e8f
shreya-212 <shreyaa.srs@gmail.com>
2026-04-01 21:19:16 +0530

    add sliding window

--- 06.Sliding-Window_and_Two-pointers/longest_substring_k_distinct_char.py
+++ 06.Sliding-Window_and_Two-pointers/longest_substring_k_distinct_char.py
@@ -1,4 +1,4 @@
-
+# Given a string s and an integer k.Find the length of the longest substring with at most k distinct characters.
 
 
 
@@ -9,10 +9,31 @@ class Solution:
         for i in range(len(s)):
             check=set()
             for j in range(i,len(s)):
-                if s[j] not in check:
-                    check.add(s[j])
+                check.add(s[j])
                 if len(check)>k:
                     break
                 if len(check)==k:
                     max_len=max(max_len,j-i+1)
+        return max_len
+
+
+
+
+
+
+#Optimal solution  -Time complexity:O(n)  ,space complexity:O(k)
+class Solution:
+    def kDistinctChar(self, s, k):
+        max_len=0
+        left=0
+        freq={}
+        for right in range(len(s)):
+            freq[s[right]]=freq.get(s[right],0)+1
+            while len(freq)>k:
+                freq[s[left]]-=1
+                if freq[s[left]]==0:
+                    del freq[s[left]]
+                left+=1
+            if len(freq)==k:
+                max_len=max(max_len,right-left+1)
         return max_len
