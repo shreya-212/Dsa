28c66475a41c4e4a973cfa0e08fb86498a77888f
shreya-212 <shreyaa.srs@gmail.com>
2026-08-11 09:25:10 +0530

    add linked list

---
+++ 07.LinkedList/linked_list-I/linked_list_cycle_II.py
@@ -0,0 +1,34 @@
+# Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+
+
+#Brute force solution  -Time complexity:O(n)  ,space complexity:O(1)
+class Solution(object):
+    def detectCycle(self, head):
+        node_check={}
+        current=head
+        while current:
+            if current in node_check:
+                return current
+            node_check[current]=1
+            current=current.next
+        return None
+
+
+
+
+
+#Optimal solution  -Time complexity:o(n)  ,space complexity:o(1)
+class Solution(object):
+    def detectCycle(self, head):
+        slow=fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                slow=head
+                while slow!=fast:
+                    slow=slow.next
+                    fast=fast.next
+                return slow
+        return None
