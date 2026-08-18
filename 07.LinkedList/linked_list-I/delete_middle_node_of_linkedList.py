eefb0dc346f3e4dc7304b8035ee8fd690f24210a
shreya-212 <shreyaa.srs@gmail.com>
2026-08-18 17:35:15 +0530

    add linked list

--- 07.LinkedList/linked_list-I/delete_middle_node_of_linkedList.py
+++ 07.LinkedList/linked_list-I/delete_middle_node_of_linkedList.py
@@ -1,9 +1,34 @@
 #You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
 
 
+
+#Brute force solution -Time complexity:O(n)  space complexity:O(1)
+class Solution(object):
+    def deleteMiddle(self, head):
+        if not head or not head.next:
+            return None
+        count=0
+        cur=head
+        while cur:
+            count+=1
+            cur=cur.next
+        mid=count/2
+        cur=head
+        for _ in range(mid-1):
+            cur=cur.next
+        cur.next=cur.next.next
+        return head
+
+
+
+
+
+
 #Optimal solution  -Time complexity:O(n)  ,space complexity:O(1)
 class Solution(object):
     def deleteMiddle(self, head):
+        if not head or not head.next:
+            return None
         slow=fast=head
         fast=fast.next.next
         while fast and fast.next:
