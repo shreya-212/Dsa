5a4b5ebc64e18db786d32de36cedae80d0ab52fc
shreya-212 <shreyaa.srs@gmail.com>
2026-08-20 18:43:32 +0530

    add linked list

--- 07.LinkedList/linked_list-I/sort_linked_list.py
+++ 07.LinkedList/linked_list-I/sort_linked_list.py
@@ -2,8 +2,7 @@
 
 
 
-
-#Brute force solution -Time complexity:O(log n)  ,space complexity:O(n)
+#Brute force solution -Time complexity:O(n log n)  ,space complexity:O(n)
 class Solution(object):
     def sortList(self, head):
         arr=[]
@@ -17,3 +16,46 @@ class Solution(object):
             current.val=arr[i]
             current=current.next
         return head
+
+
+
+
+#Optimal solution  -Time complexityO(n log n)  ,space complexity:O(1)
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def merge(self,l1,l2):
+        new=ListNode()
+        temp=new
+        while l1 and l2:
+            if l1.val<=l2.val:
+                temp.next=l1
+                l1=l1.next
+            else:
+                temp.next=l2
+                l2=l2.next
+            temp=temp.next
+        if l1:
+            temp.next=l1
+        else:
+            temp.next=l2
+        return new.next
+
+
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+        slow=head
+        fast=head.next
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        right=slow.next
+        slow.next=None
+        left=head
+
+        left=self.sortList(left)
+        right=self.sortList(right)
+        return self.merge(left,right)
